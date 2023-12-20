ad9205053eb1a00fe893f19b4b6efba22684ec22
Jens Winkler <jens.winkler@gbes.de>
2023-12-20 11:47:31 +0100

    added: .gitignore, maven file, Main-Class to simplify the example

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import api.API;
+
+public class Main {
+
+    static final String API_KEY = "";
+    static final String DOMAIN = "";
+    static final int PORT = -1;
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
